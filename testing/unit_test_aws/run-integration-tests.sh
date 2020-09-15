6656a08e825b45261d0609ad010a9074adb26407
Matt Wise <wsmatth@amazon.com>
2020-09-15 14:38:42 -0500

    support for RHEL based int tests

--- testing/unit_test_aws/run-integration-tests.sh
+++ testing/unit_test_aws/run-integration-tests.sh
@@ -16,6 +16,7 @@
 
 # Install venv for python3
 which apt-get && sudo apt-get install -y python3 python3-pip python3-venv || echo "Not Ubuntu, skipping"
+which yum && sudo yum install -y python3 python3-pip python3-venv || echo "Not RHEL, skipping"
 
 python3 -m venv env
 # sed -i 's/$1/${1:-}/' env/bin/activate # Fix deactivation bug '$1 unbound variable'
