3e8366b47ef205377c1161af9d1ac8cc112a337a
Siarhei Khaletski <siarhei_khaletski1@epam.com>
2020-09-18 14:07:33 +0300

    GONRG-827: Moved integration tests for GCP to v3; Testing README.md updated

--- testing/unit_test_gcp/run_test.py
+++ testing/unit_test_gcp/run_test.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 
-from unit_test_core.test_unit_service_v2 import *
+from unit_test_core.test_unit_service_v3 import *
 
 if __name__ == '__main__':
     unittest.main()
