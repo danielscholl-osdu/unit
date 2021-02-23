92a99562ee441134f144f3e9e98d6f8969047837
Anastasiia Gelmut <anastasiia_gelmut@epam.com>
2021-02-23 00:45:51 -0500

    GONRG-827: Moved integration tests for GCP to v3; Testing README.md updated

--- provider/unit-gcp/unit-gae/src/main/java/org/opengroup/osdu/unitservice/UomGaeApplication.java
+++ provider/unit-gcp/unit-gae/src/main/java/org/opengroup/osdu/unitservice/UomGaeApplication.java
@@ -19,8 +19,10 @@ package org.opengroup.osdu.unitservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
+@ComponentScan({"org.opengroup.osdu"})
 @SpringBootApplication
 public class UomGaeApplication {
 
