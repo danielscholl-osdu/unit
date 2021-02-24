4a04a754d2a377b0aec7f952ed0616ab4eb941ac
Stanislav Riabokon <Stanislav_Riabokon@epam.com>
2021-02-24 17:20:48 +0400

    Removed debug stub.

--- unit-core/src/main/java/org/opengroup/osdu/unitservice/middleware/AuthenticationRequestFilter.java
+++ unit-core/src/main/java/org/opengroup/osdu/unitservice/middleware/AuthenticationRequestFilter.java
@@ -59,7 +59,6 @@ public class AuthenticationRequestFilter extends OncePerRequestFilter {
             String message = String.format("User authenticated | User: %s", groups.getMemberEmail());
             authenticate(groups);
             logger.info(message);
-            httpServletRequest.setAttribute("user", "stas");
             filterChain.doFilter(httpServletRequest, httpServletResponse);
         } catch (EntitlementsException e) {
             String message = String.format("User not authenticated. Response: %s", e.getHttpResponse());
