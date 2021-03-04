08d9f8ed1d720727d29cb2450bb66f186e679f00
gonagar1 <gonagar1@in.ibm.com>
2021-03-04 12:01:51 +0530

    ibm implementation & lib upgrade

--- provider/unit-ibm/unit-ocp/src/main/java/org/opengroup/osdu/unitservice/security/SecurityConfig.java
+++ provider/unit-ibm/unit-ocp/src/main/java/org/opengroup/osdu/unitservice/security/SecurityConfig.java
@@ -1,33 +1,88 @@
 package org.opengroup.osdu.unitservice.security;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.opengroup.osdu.unitservice.middleware.AuthenticationRequestFilter;
+import org.opengroup.osdu.unitservice.util.AppError;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements AccessDeniedHandler, AuthenticationEntryPoint {
 
+    private AuthenticationRequestFilter authFilter;
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    
+    private static final String[] AUTH_WHITELIST = {
+            "/",
+            "/v2/api-docs",
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/_ah/**",
+            "/configuration/security",
+            "/configuration/ui",
+    };
+    
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 
-		
-		//AuthenticationRequestFilter authhenticationFilter = new AuthenticationRequestFilter( "" , handlerExceptionResolver);
-		
         http.csrf().disable()
-        //.sessionManagement().sessionCreationPolicy(STATELESS)
-        //.and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+        .and()
         .authorizeRequests()
-        //.antMatchers(ALLOWED_URLS).permitAll()
-        .antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**").permitAll()
+        .antMatchers(AUTH_WHITELIST).permitAll()
         .anyRequest().authenticated()
         .and()
-        .oauth2ResourceServer().jwt();        
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+	
+	public SecurityConfig(@Value("${osdu.entitlement.url}") String entitlementsUrl, HandlerExceptionResolver handlerExceptionResolver) {
+        authFilter = new AuthenticationRequestFilter(entitlementsUrl, handlerExceptionResolver);
+    }
+
+	@Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException {
+        writeUnauthorizedError(httpServletResponse);
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        writeUnauthorizedError(response);
+    }
+
+    private static void writeUnauthorizedError(HttpServletResponse response) throws IOException {
+        AppError appError = AppError.builder()
+                .code(HttpStatus.UNAUTHORIZED.value())
+                .message("The user is not authorized to perform this action")
+                .reason("Unauthorized")
+                .build();
+        String body = OBJECT_MAPPER.writeValueAsString(appError);
+
+        PrintWriter out = response.getWriter();
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(body);
+        out.flush();
     }
 }
