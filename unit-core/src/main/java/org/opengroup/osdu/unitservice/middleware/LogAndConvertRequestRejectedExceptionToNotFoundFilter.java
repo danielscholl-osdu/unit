973d17195f2b61457ca463ce9265f0f6e658fbc5
Yifan Ye <yye5@slb.com>
2021-08-24 19:32:08 +0000

    Fix the logger that causing the recent 500 errors in unit-service

--- unit-core/src/main/java/org/opengroup/osdu/unitservice/middleware/LogAndConvertRequestRejectedExceptionToNotFoundFilter.java
+++ unit-core/src/main/java/org/opengroup/osdu/unitservice/middleware/LogAndConvertRequestRejectedExceptionToNotFoundFilter.java
@@ -1,40 +1,27 @@
 package org.opengroup.osdu.unitservice.middleware;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHeaders;
 import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.Ordered;
 import org.springframework.security.web.firewall.RequestRejectedException;
+import org.springframework.security.web.firewall.RequestRejectedHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class LogAndConvertRequestRejectedExceptionToNotFoundFilter extends GenericFilterBean {
+@RequiredArgsConstructor
+public class LogAndConvertRequestRejectedExceptionToNotFoundFilter implements RequestRejectedHandler {
 
-    @Autowired
-    private JaxRsDpsLog log;
+    public static final String REQUEST_REJECTED_REASON_MESSAGE = "request_rejected: remote=%s, user_agent=%s, request_url=%s";
+
+    private final JaxRsDpsLog log;
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        try {
-            chain.doFilter(req, res);
-        } catch (RequestRejectedException e) {
-            HttpServletRequest request = (HttpServletRequest) req;
-            HttpServletResponse response = (HttpServletResponse) res;
-            log.error(String.format("request_rejected: remote=%s, user_agent=%s, request_url=%s",
-                request.getRemoteHost(), request.getHeader(HttpHeaders.USER_AGENT), request.getRequestURL()), e);
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
+    public void handle(HttpServletRequest request, HttpServletResponse response, RequestRejectedException requestRejectedException) throws IOException {
+        log.error(String.format(REQUEST_REJECTED_REASON_MESSAGE,
+                request.getRemoteHost(), request.getHeader(HttpHeaders.USER_AGENT), request.getRequestURL()), requestRejectedException);
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
     }
 }
