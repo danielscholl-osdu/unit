4a788eb0e1dbd6ca8aa31b13bb551f9209a5f0f5
mburnie <mburnie@amazon.com>
2021-12-08 21:17:53 +0000

    Merge branch 'dev' into fix-HttpSecurity and removed any request permission

--- provider/unit-aws/src/main/java/org/opengroup/osdu/unitservice/security/AuthSecurityConfig.java
+++ provider/unit-aws/src/main/java/org/opengroup/osdu/unitservice/security/AuthSecurityConfig.java
@@ -86,7 +86,6 @@ public class AuthSecurityConfig extends WebSecurityConfigurerAdapter implements 
                 .and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
     }
