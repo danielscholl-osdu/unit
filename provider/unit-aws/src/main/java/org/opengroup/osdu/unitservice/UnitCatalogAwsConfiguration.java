c28e00b15af5050a6f8544350076e6bfd2224aa0
Yunhua Koglin <kogliny@amazon.com>
2023-09-27 16:17:55 +0000

    fix code smell

--- provider/unit-aws/src/main/java/org/opengroup/osdu/unitservice/UnitCatalogAwsConfiguration.java
+++ provider/unit-aws/src/main/java/org/opengroup/osdu/unitservice/UnitCatalogAwsConfiguration.java
@@ -1,23 +1,19 @@
-// Copyright © 2020 Amazon Web Services
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright © 2020 Amazon Web Services
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
 
 package org.opengroup.osdu.unitservice;
 
-// import org.opengroup.osdu.core.aws.s3.S3Config;
-// import com.amazonaws.services.s3.AmazonS3;
-// import com.amazonaws.services.s3.model.S3Object;
-// import com.amazonaws.services.s3.model.GetObjectRequest;
 
 import org.opengroup.osdu.unitservice.model.CatalogImpl;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,7 +21,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.logging.Logger;
 
 import java.io.FileReader;
 
@@ -33,7 +28,6 @@ import java.io.FileReader;
 @PropertySource("classpath:swagger.properties")
 public class UnitCatalogAwsConfiguration {
 
-    private static final Logger log = Logger.getLogger(UnitCatalogAwsConfiguration.class.getName());
 
     @Bean
     public CatalogImpl catalogImpl(@Value("${osdu.unit.catalog.filename}") String location) throws Exception {
