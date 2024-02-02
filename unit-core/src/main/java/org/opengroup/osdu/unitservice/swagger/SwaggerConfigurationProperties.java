3eaef110ee7d3d4cf256d87e808f71bb09244cd2
Anthony Ittiyera Vazhappilly <avazhappilly@microsoft.com>
2024-02-01 23:04:52 -0800

    fix missing prefix

--- unit-core/src/main/java/org/opengroup/osdu/unitservice/swagger/SwaggerConfigurationProperties.java
+++ unit-core/src/main/java/org/opengroup/osdu/unitservice/swagger/SwaggerConfigurationProperties.java
@@ -14,74 +14,10 @@ public class SwaggerConfigurationProperties {
     private String apiTitle;
     private String apiDescription;
     private String apiVersion;
-    private String contactName;
-    private String contactEmail;
-    private String licenseName;
-    private String licenseUrl;
+    private String apiContactName;
+    private String apiContactEmail;
+    private String apiLicenseName;
+    private String apiLicenseUrl;
     private boolean apiServerFullUrlEnabled;
-
-    public String getApiTitle() {
-        return apiTitle;
-    }
-
-    public void setApiTitle(String apiTitle) {
-        this.apiTitle = apiTitle;
-    }
-
-    public String getApiDescription() {
-        return apiDescription;
-    }
-
-    public void setApiDescription(String apiDescription) {
-        this.apiDescription = apiDescription;
-    }
-
-    public String getApiVersion() {
-        return apiVersion;
-    }
-
-    public void setApiVersion(String apiVersion) {
-        this.apiVersion = apiVersion;
-    }
-
-    public String getContactName() {
-        return contactName;
-    }
-
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getLicenseName() {
-        return licenseName;
-    }
-
-    public void setLicenseName(String licenseName) {
-        this.licenseName = licenseName;
-    }
-
-    public String getLicenseUrl() {
-        return licenseUrl;
-    }
-
-    public void setLicenseUrl(String licenseUrl) {
-        this.licenseUrl = licenseUrl;
-    }
-
-    public boolean isApiServerFullUrlEnabled() {
-        return apiServerFullUrlEnabled;
-    }
-
-    public void setApiServerFullUrlEnabled(boolean apiServerFullUrlEnabled) {
-        this.apiServerFullUrlEnabled = apiServerFullUrlEnabled;
-    }
 
 }
